c5e51b5c773683adb4f9b42a3aa58bbe142a5048
vartikus1337 <pasha_klevtsov@vk.com>
2025-12-02 20:40:40 +0300

    added a hint about using Tab

--- src/Views/NiriView.tsx
+++ src/Views/NiriView.tsx
@@ -1,26 +1,36 @@
 import { usePkgsFile, useView } from "@/Contexts";
 import { ListInput, ListInputItem } from "@/Components/ListInput";
+import { Box, Text } from "ink";
 
 export const NiriView = () => {
   const { changeView } = useView();
   const { data, toggleDisablePkg } = usePkgsFile();
 
   return (
-    <ListInput onExit={() => changeView("start")}>
-      {data.NiriPkgs.map((pkg) => (
-        <ListInputItem
-          key={typeof pkg === "string" ? pkg : pkg.name}
-          onTabKey={() =>
-            toggleDisablePkg(
-              "NiriPkgs",
-              typeof pkg === "string" ? pkg : pkg.name,
-            )
-          }
-          disabled={typeof pkg === "string" ? false : pkg.disabled}
-        >
-          {typeof pkg === "string" ? pkg : pkg.name}
-        </ListInputItem>
-      ))}
-    </ListInput>
+    <Box
+      display="flex"
+      width={"100%"}
+      flexDirection="column"
+      alignItems="center"
+      gap={2}
+    >
+      <Text color={"grey"}>press Tab to disable the package installation.</Text>
+      <ListInput onExit={() => changeView("start")}>
+        {data.NiriPkgs.map((pkg) => (
+          <ListInputItem
+            key={typeof pkg === "string" ? pkg : pkg.name}
+            onTabKey={() =>
+              toggleDisablePkg(
+                "NiriPkgs",
+                typeof pkg === "string" ? pkg : pkg.name,
+              )
+            }
+            disabled={typeof pkg === "string" ? false : pkg.disabled}
+          >
+            {typeof pkg === "string" ? pkg : pkg.name}
+          </ListInputItem>
+        ))}
+      </ListInput>
+    </Box>
   );
 };
